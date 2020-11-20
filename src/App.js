c8b133e06a424a5f27cdfab27d44c136147daa30
Matthew Gerard Wilson <2332309w@student.gla.ac.uk>
2020-11-20 10:43:31 +0000

    Made a mockup of the wireframe using material-ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,91 @@
+import {
+  Grid,
+  Typography,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    textAlign: "center",
+    padding: theme.spacing(3),
+  },
+  content: {
+    padding: theme.spacing(3),
+  },
+  column: {
+    textAlign: "center",
+  },
+  columnTitle: {
+    paddingBottom: theme.spacing(3),
+  },
+  image: {
+    maxWidth: "75%",
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Typography variant="h5" component="h1" className={classes.title}>
+        COVIDUALISE: A Visualisation Tool For COVID-19 Infection Rates
+      </Typography>
+
+      <Grid container component="main" className={classes.content}>
+        <Grid item xs={2} className={classes.column}>
+          <Typography
+            variant="h6"
+            component="h2"
+            className={classes.columnTitle}
+          >
+            Preventative Measures
+          </Typography>
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Foo" />
+            <FormControlLabel control={<Checkbox />} label="Bar" />
+            <FormControlLabel control={<Checkbox />} label="One" />
+            <FormControlLabel control={<Checkbox />} label="Two" />
+            <FormControlLabel control={<Checkbox />} label="Three" />
+            <FormControlLabel control={<Checkbox />} label="Four" />
+          </FormGroup>
+        </Grid>
+
+        <Grid item xs={8} className={classes.column}>
+          <Typography
+            variant="h6"
+            component="h2"
+            className={classes.columnTitle}
+          >
+            Visualisation and Graphing
+          </Typography>
+          <img
+            src="https://i.redd.it/ylu4wlgozgt51.jpg"
+            alt=""
+            className={classes.image}
+          />
+        </Grid>
+
+        <Grid item xs={2} className={classes.column}>
+          <Typography
+            variant="h6"
+            component="h2"
+            className={classes.columnTitle}
+          >
+            Context and Factualisation
+          </Typography>
+          <Typography>
+            Lorem ipsum dolor sit amet et delectus accommodare his consul
+            copiosae legendos at vix ad putent delectus delicata usu. Vidit
+            dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum
+            an. Per facer affert ut. Mei iisque mentitum
+          </Typography>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
